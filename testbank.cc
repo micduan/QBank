9092fd00ace4aa69d2cf56854fb6168499cdffd5
micduan <michael.duan123@gmail.com>
2018-03-12 00:33:41 -0400

    use regex to create questions

--- testbank.cc
+++ testbank.cc
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <regex>
 
 testbank::testbank(std::string myPath) {
 	std::ifstream inFile;
@@ -12,11 +13,100 @@ testbank::testbank(std::string myPath) {
 
 
 	std::string test;
-
-	Question myQuestion;
 	while (inFile >> test) { 
 		// get rid of the text prior to any questions
 		if (test == "1)") break;
+	}
+
+	while (true) {
+		Question myQuestion;	
+		std::string current = "question";
+		std::string question;
+		std::string optionA;
+		std::string optionB;
+		std::string optionC;
+		std::string optionD;
+		std::string optionE;
+		std::string difficulty;
+		std::string answer;
+		std::string topic;
+
+		std::string any_number = "^([0-9]*)\\)$";
+		std::regex nextQuestion(any_number);
+		while (inFile >> test) {
+			if (regex_match(test, nextQuestion)) {
+				Question myQuestion(optionA, optionB, optionC, optionD, optionE, stoi(difficulty), answer, topic);
+				this->myQuestions.push_back(myQuestion);
+				break;
+			}
+
+			if (test == "A)") {
+				current = "optionA";
+				continue;
+			}
+
+			if (test == "B)") {
+				current = "optionB";
+				continue;
+			}
+
+			if (test == "C)") {
+				current = "optionC";
+				continue;
+			}
+
+			if (test == "D)") {
+				current = "optionD";
+				continue;
+			}
+
+			if (test == "E)") {
+				current = "optionE";
+				continue;
+			}
+
+			if (test == "Answer:") {
+				current = "answer";
+				continue;
+			}
+
+			if (test == "Diff:") {
+				current = "difficulty";
+				continue;
+			}
+
+			if (test == "Topic:") {
+				current = "topic";
+				continue;
+			}
+
+			if (current == "question") {
+				question += " " + test;
+			} else if (current == "optionA") {
+				optionA += " " + test;
+			} else if (current == "optionB") {
+				optionB += " " + test;
+			} else if (current == "optionC") {
+				optionC += " " + test;
+			} else if (current == "optionD") {
+				optionD += " " + test;
+			} else if (current == "optionE") {
+				optionE += " " + test;
+			} else if (current == "answer") {
+				answer += " " + test;
+			} else if (current == "difficulty") {
+				difficulty += " " + test;
+			} else if (current == "topic") {
+				topic += " " + topic;
+			}
+		}
+
+		if (inFile.eof()) {
+			Question myQuestion(optionA, optionB, optionC, optionD, optionE, stoi(difficulty), answer, topic);
+			this->myQuestions.push_back(myQuestion);
+			break;
+		}
+
 	}
 	inFile.close();
 }
